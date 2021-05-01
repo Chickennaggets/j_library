0a65195071a1d455bf8c2a6887fcbef64d775e4c
Chickennaggets <leonovstarget@gmail.com>
2021-05-01 15:38:01 +0200

    Исправил пароль от доступа к базе данных

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ define('ROOT_FOLDER', '../');
 
 define('DB_SERVER', 'localhost');
 define('DB_NAME', 'library');
-define('DB_USER', 'library');
-define('DB_PAS', 'library');
+define('DB_USER', 'root');
+define('DB_PAS', '');
 
 ?>
