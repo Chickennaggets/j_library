d406fa04720ec1c475dd8bdb3c12ca88ce0c0ea6
Chickennaggets <leonovstarget@gmail.com>
2021-05-05 20:31:41 +0200

    Изменил код на функции для шапок и панели навигации

--- folders.php
+++ folders.php
@@ -1,36 +1,12 @@
 <html>
-<head>
-    <link rel="stylesheet" href="style.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
-    <div class="header">
-        <h1 class="zagl">Biblioteka Chóru Katedralnego</h1>
-    </div>
-    
-</head>
+<?php
+    print_header();
+?>
 <body>
     <table>
-         <tr><td style="width: 180px;" valign="top">
-             <div class="navigator">
-                    <?php 
-                 session_start();
-                 echo $_SESSION["online_login"]."<br>";
-                 if($_SESSION["root"]){
-                     echo "(Administrator)<br><br>";
-                 }else{
-                     echo "(Użytkownik)<br><br>";
-                 }?>
-             <a href="main.php" class = "a">Główna</a><br>
-                 <?php
-                 if($_SESSION["root"]){
-                     echo "<a href='add_song.php' class = 'a'>Nowy utwór</a><br>";
-                     echo "<a href='folders.php' class = 'a'>Teczki</a><br>";
-                     echo "<a href='users.php' class = 'a'>Użytkowniki</a><br>";
-                 }                
-                 ?>
-              <a href="authorization.html" class = "a">Wyloguj</a><br>
-             </div>
-             </td>
+        <tr><?php
+                print_nav_menu();
+            ?>
              <td valign="top" style="width: 80%;">
                  <div class="p_navigator" style="width: 100%;">
            <a class = "a" href="add_fold.php">Nowa teczka</a><br><br>    
